14f3c295f3dcff1045f8635dace016ba10e417a6
shruthiswaminathan1 <72410785+shruthiswaminathan1@users.noreply.github.com>
2021-05-10 22:20:45 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+export default function App() {
+  return (
+    <View style={{flex: 1}}>
+      <HomeScreen/>
+    </View>
+  );
+}
+
+
+
+
+npm install react react-native react-navigation react-navigation-tabs expo-permissions expo-barcode-scanner react-native-gesture-handler react-native-reanimated react-native-screens
+react-native-web react-navigation
+react-navigation-tabs
+Expo-av react-native-elements 
